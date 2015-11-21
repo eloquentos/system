355534e4d82e92c85b244aa7641b7ec34b37e534
Chris Harvey <chris@chrisnharvey.com>
2015-11-21 13:20:22 +0000

    Don't change runlevel

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -20,4 +20,3 @@ sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
 systemctl enable pacman-init.service choose-mirror.service
 systemctl enable gdm.service
-systemctl set-default multi-user.target

bd1da559e1827fe1ee7c6c03fd75cafd6d48128d
Chris Harvey <chris@chrisnharvey.com>
2015-12-05 17:43:00 +0000

    Copy our custom initcpio scripts into the work directory

--- build.sh
+++ build.sh
@@ -75,6 +75,10 @@ make_setup_mkinitcpio() {
         cp /usr/lib/initcpio/hooks/${_hook} ${work_dir}/${arch}/airootfs/etc/initcpio/hooks
         cp /usr/lib/initcpio/install/${_hook} ${work_dir}/${arch}/airootfs/etc/initcpio/install
     done
+
+    cp ${script_path}/initcpio/hooks/* ${work_dir}/${arch}/airootfs/etc/initcpio/hooks
+    cp ${script_path}/initcpio/install/* ${work_dir}/${arch}/airootfs/etc/initcpio/install
+
     sed -i "s|/usr/lib/initcpio/|/etc/initcpio/|g" ${work_dir}/${arch}/airootfs/etc/initcpio/install/archiso_shutdown
     cp /usr/lib/initcpio/install/archiso_kms ${work_dir}/${arch}/airootfs/etc/initcpio/install
     cp /usr/lib/initcpio/archiso_shutdown ${work_dir}/${arch}/airootfs/etc/initcpio

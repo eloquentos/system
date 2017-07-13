95f2357077443ab64eef74abbe12bb31a5ce7b9b
Unknown <chris@chrisnharvey.com>
2017-07-13 18:42:22 +0100

    Deployment

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sshpass -e sftp -oBatchMode=no -oStrictHostKeyChecking=no -b - nimbusoft@frs.sourceforge.net << !
-   cd /home/pfs/project/eloquentos/system/ci
-   put out/*.iso ${CI_JOB_ID}.iso
+   cd /home/pfs/project/eloquentos/system/releases
+   put out/*.iso ${CI_COMMIT_TAG}.iso
    bye
 !

d53d15aeaf046c4dae521d469b035c5642483676
Unknown <chris@chrisnharvey.com>
2017-07-14 01:40:19 +0100

    Symlink release instead of hardlink

--- release.sh
+++ release.sh
@@ -2,6 +2,6 @@
 
 sshpass -e sftp -oBatchMode=no -oStrictHostKeyChecking=no -b - nimbusoft@frs.sourceforge.net << !
    cd /home/pfs/project/eloquentos/system
-   ln ci/${CI_PIPELINE_ID}.iso releases/${CI_COMMIT_TAG}.iso
+   symlink ci/${CI_PIPELINE_ID}.iso releases/${CI_COMMIT_TAG}.iso
    bye
 !
